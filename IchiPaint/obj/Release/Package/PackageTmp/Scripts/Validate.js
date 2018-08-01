e9df46e971f523620e3dd5e7f49f4f5fa9486c4c
dangtq72 <dangtq72@gmail.com>
2018-08-01 22:14:49 +0700

    dangtq sửa theo yc khách hàng

--- IchiPaint/obj/Release/Package/PackageTmp/Scripts/Validate.js
+++ IchiPaint/obj/Release/Package/PackageTmp/Scripts/Validate.js
@@ -1,7 +1,7 @@
 ﻿function isNumber(evt) {
     evt = (evt) ? evt : window.event;
     var charCode = (evt.which) ? evt.which : evt.keyCode;
-    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+    if (charCode > 31 && (charCode < 48 || charCode > 57) && charCode !== 46) {
         return false;
     }
     return true;
